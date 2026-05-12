a6ede8c7c4f9685a0ead44c70973127c7fe13631
Victor Antonio Mamani Yaringaño <victoryari61@gmail.com>
2026-05-11 23:01:55 -0500

    Debug: Habilitar visualización de errores PHP

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
+// Activar errores para depuración en Vercel
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Forward Vercel requests to the public/index.php of Laravel
 require __DIR__ . '/../public/index.php';
